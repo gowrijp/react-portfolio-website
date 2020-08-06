3ed3a73860b599788cdf6e0d916085a80e048ff9
Gowri <gowrijp.16@gmail.com>
2020-08-07 02:47:59 +0530

    Updates

---
+++ precache-manifest.32f7b00b9186c4df856d4c0ad97468df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a0ca3ad87d47fec68cb16a933e50e15",
+    "url": "/react-portfolio-website/index.html"
+  },
+  {
+    "revision": "a1acb40a52fb30a7e557",
+    "url": "/react-portfolio-website/static/css/main.34434ca8.chunk.css"
+  },
+  {
+    "revision": "be65e3ead66f54eff499",
+    "url": "/react-portfolio-website/static/js/2.283ad937.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio-website/static/js/2.283ad937.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1acb40a52fb30a7e557",
+    "url": "/react-portfolio-website/static/js/main.571e2f91.chunk.js"
+  },
+  {
+    "revision": "feb6c3520dfd6a2c561d",
+    "url": "/react-portfolio-website/static/js/runtime-main.95c10203.js"
+  }
+]);
